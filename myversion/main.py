08a22ef64d37acb1d403f15bdfd9729f7aaf8eca
zjf <zjf@epoint.com.cn>
2021-03-07 21:51:50 +0800

    v1

---
+++ myversion/main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import random
+import time
+import tkinter as tk
+from PIL import ImageTk, Image
+
+
+tk = Tk()
+
+canvas = Canvas(tk, width=800,height=600)
+canvas.pack()
+tk.mainloop()
