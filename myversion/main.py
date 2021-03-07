c9d2bae2f68e7a55b329b47dbb7d77116f5b8fd2
zjf <zjf@epoint.com.cn>
2021-03-07 22:00:59 +0800

    v3 简单的动

--- myversion/main.py
+++ myversion/main.py
@@ -10,9 +10,36 @@ tk = Tk()
 canvas = Canvas(tk, width=800,height=600)
 canvas.pack()
 
+backimg = ImageTk.PhotoImage(Image.open("img/scene/map0.jpg"))
+obj_back = canvas.create_image(0,0,anchor=NW,image=backimg)  
+
 
 img1 = ImageTk.PhotoImage(Image.open("img/Sprite0/0-0-0.png"))
-obj = canvas.create_image(200,370,anchor=NW,image=img1)  
+img2 = PhotoImage(file = "img/Sprite0/0-0-1.png")
+img3 = PhotoImage(file = "img/Sprite0/0-0-2.png")
+img4 = PhotoImage(file = "img/Sprite0/0-0-3.png")
+imgs =[img1,img2,img3,img4]
+obj = canvas.create_image(200,370,anchor=NW,image=imgs[0])  
 
 
+p =0 
+index = 0
+
+def loop():
+    global p,index
+
+    index = index + 1
+    if index == 15:
+        index = 0
+            
+    if index ==0 :
+        p = p+1
+        if p == 3:
+            p = 0
+
+    canvas.itemconfig(obj, image=imgs[p])
+    
+    canvas.after(10, loop)
+
+loop()
 tk.mainloop()
