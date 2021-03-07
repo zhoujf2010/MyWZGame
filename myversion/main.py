2106563069dd67e557f362952bf2f182fa69e3be
zjf <zjf@epoint.com.cn>
2021-03-07 21:59:41 +0800

    v2 show image

--- myversion/main.py
+++ myversion/main.py
@@ -9,4 +9,10 @@ tk = Tk()
 
 canvas = Canvas(tk, width=800,height=600)
 canvas.pack()
+
+
+img1 = ImageTk.PhotoImage(Image.open("img/Sprite0/0-0-0.png"))
+obj = canvas.create_image(200,370,anchor=NW,image=img1)  
+
+
 tk.mainloop()
